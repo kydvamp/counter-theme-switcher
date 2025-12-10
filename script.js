eddb40a835e80e5a159a8fdb26ebf22692ad510a
codedbyky <kyyyagullano@gmail.com>
2025-12-10 18:33:31 +0800

    Implement dark mode

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let number = 0;
+let darkmode = false;
 
 const incrementButton = document.getElementById('increment-button');
 const decrementButton = document.getElementById('decrement-button');
 const numberDisplay = document.getElementById('number-display');
 const retryButton = document.getElementById('retry-button');
-const spacer = document.querySelector('.spacer');
+const darkmodeButton = document.getElementById('dark-mode-button');
+const lightmodeButton = document.getElementById('light-mode-button');
 
 incrementButton.addEventListener('click', () => {
     number += 1;
@@ -37,6 +39,25 @@ function updateDisplay() {
 }
 updateDisplay();
 
+darkmodeButton.addEventListener('click', () => {
+    document.body.classList.add('dark-mode');
+    darkmode = true;
+    darkmodeButton.style.display = 'none';
+    lightmodeButton.style.display = 'inline-block';
+});
+
+lightmodeButton.addEventListener('click', () => {
+    document.body.classList.remove('dark-mode');
+    darkmode = false;
+    darkmodeButton.style.display = 'inline-block';
+    lightmodeButton.style.display = 'none';
+});
+
+if (darkmode) {
+    document.body.classList.add('dark-mode');
+} else {
+    document.body.classList.remove('dark-mode');
+}
 
 
 
